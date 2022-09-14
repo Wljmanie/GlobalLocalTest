405c530a81f00978c88345d7e5eea41fde9d1c45
Wljmanie <45335409+Wljmanie@users.noreply.github.com>
2022-09-14 10:52:21 +0200

    Added backspace functionality when nothing is selected.

--- GlobalLocalTest/Controllers/HomeController.cs
+++ GlobalLocalTest/Controllers/HomeController.cs
@@ -37,14 +37,7 @@ namespace GlobalLocalTest.Controllers
         }
 
 
-        [HttpPost]
-        public IActionResult CultureManagement(string culture, string returnUrl)
-        {
-            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
-                new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30)});
-
-            return LocalRedirect(returnUrl);
-        }
+        
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
